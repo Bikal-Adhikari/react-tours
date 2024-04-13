9952d18cac0c87f9bd6763daf7d885f3fb514366
Bikal <bikaladhikari5@gmail.com>
2024-04-13 16:41:52 +1000

    fetching tour api and rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 
 const url = "https://course-api.com/react-tours-project";
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
 
   const fetchTours = async () => {
     // will use later
@@ -18,14 +26,32 @@ function App() {
       console.log(error);
     }
   };
-
   useEffect(() => {
     fetchTours();
   }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
-    <>
-      <div>Tour Travel</div>
-    </>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
