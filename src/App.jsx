fe1cfaa5bf91d66d72a1d3de2e222104a78e89d7
Bikal <bikaladhikari5@gmail.com>
2024-04-13 16:40:19 +1000

    fetching tour api

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
-import "./App.css";
+import { useEffect } from "react";
 
+const url = "https://course-api.com/react-tours-project";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    // will use later
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
   return (
     <>
       <div>Tour Travel</div>
